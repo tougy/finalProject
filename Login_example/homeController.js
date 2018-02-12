b4f56c73f64b2c6c587ff360ea2a5a17ca9ef5fe
tougy <34806660+tougy@users.noreply.github.com>
2018-02-12 09:33:36 +0200

    Initial commit

---
+++ Login_example/homeController.js
@@ -0,0 +1,8 @@
+//homeController.js
+var app = angular.module('myApp');
+ 
+  app.controller('HomeController', 
+  function($scope, $rootScope, $stateParams, $state, LoginService) {
+    $scope.user = $rootScope.userName;
+    
+  });
