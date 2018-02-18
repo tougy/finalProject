466045048ac64679663079ebc5ef1c2f8784e280
tougy <34806660+tougy@users.noreply.github.com>
2018-02-18 20:42:08 +0200

    Login version Sunday 20.40

--- Angular/services/loginService.js
+++ Angular/services/loginService.js
@@ -1,19 +1,25 @@
 //loginService.js
-var app = angular.module('yapp');
-  
-  app.factory('LoginService', function() {
-    var admin = 'admin';
-    var pass = 'password';
+angular.module('yapp').factory('LoginService', function() {
+/*     var admin = 'admin';
+    var pass = 'password'; */
     var isAuthenticated = false;
-    
+    var success = false ;
+    // var users = [];
+
     return {
-      login : function(username, password) {
-        isAuthenticated = username === admin && password === pass;
+      login : function(username, password, users) {
+        users.forEach(element => {
+        isAuthenticated = username === element.name && password === element.pass;
+        if (isAuthenticated) { 
+          success = true ;
+          }
+        });
+        isAuthenticated = success;
+        success = false ;
         return isAuthenticated;
       },
       isAuthenticated : function() {
         return isAuthenticated;
       }
-    };
-    
+    };  
   });
---
+++ TODELETE/Login_example/loginService.js
@@ -0,0 +1,19 @@
+//loginService.js
+var app = angular.module('myApp');
+  
+  app.factory('LoginService', function() {
+    var admin = 'admin';
+    var pass = 'password';
+    var isAuthenticated = false;
+    
+    return {
+      login : function(username, password) {
+        isAuthenticated = username === admin && password === pass;
+        return isAuthenticated;
+      },
+      isAuthenticated : function() {
+        return isAuthenticated;
+      }
+    };
+    
+  });
